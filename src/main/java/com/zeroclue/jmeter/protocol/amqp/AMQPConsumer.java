d9b0864a0183db267a404f9d475d7dc2d64e4e58
Aliaksandr Belik <alies.belik@gmail.com>
2021-10-29 04:13:22 +0300

    Fix no header in reply-to message (see jlavallee#41)

--- src/main/java/com/zeroclue/jmeter/protocol/amqp/AMQPConsumer.java
+++ src/main/java/com/zeroclue/jmeter/protocol/amqp/AMQPConsumer.java
@@ -365,13 +365,18 @@ public class AMQPConsumer extends AMQPSampler implements Interruptible, TestStat
             .append(": ")
             .append(delivery.getEnvelope().getRoutingKey())
             .append("\n");
-        sb.append(DELIVERY_TAG_PARAMETER).append(": ").append(delivery.getEnvelope().getDeliveryTag()).append("\n");
+        sb.append(DELIVERY_TAG_PARAMETER)
+            .append(": ")
+            .append(delivery.getEnvelope().getDeliveryTag())
+            .append("\n");
 
-        for (String key : headers.keySet()) {
-            sb.append(key)
-                .append(": ")
-                .append(headers.get(key))
-                .append("\n");
+        if (headers != null) {
+            for (String key : headers.keySet()) {
+                sb.append(key)
+                    .append(": ")
+                    .append(headers.get(key))
+                    .append("\n");
+            }
         }
 
         return sb.toString();
